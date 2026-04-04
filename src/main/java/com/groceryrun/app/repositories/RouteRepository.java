f524bb3747fa5879ae6594913330261dedf930c2
itsreverence <github.shakily561@passmail.net>
2026-04-04 15:21:17 -0500

    align entities with updated class diagram

--- src/main/java/com/groceryrun/app/repositories/RouteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.groceryrun.app.repositories;
-
-import com.groceryrun.app.entities.Route;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RouteRepository extends JpaRepository<Route, Integer> {
-}
