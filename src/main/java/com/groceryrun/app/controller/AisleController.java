c9bff94611e404ab57ee0f4cec33bec97df82de1
itsreverence <158013050+itsreverence@users.noreply.github.com>
2026-04-07 23:17:26 -0400

    add aisles page (#22)

--- src/main/java/com/groceryrun/app/controller/AisleController.java
+++ src/main/java/com/groceryrun/app/controller/AisleController.java
@@ -3,6 +3,7 @@ package com.groceryrun.app.controller;
 import com.groceryrun.app.dto.aisle.AisleCategoriesChangeDTO;
 import com.groceryrun.app.dto.aisle.AisleDTO;
 import com.groceryrun.app.dto.aisle.AisleStoreChangeDTO;
+import com.groceryrun.app.dto.category.NewCategoryDTO;
 import com.groceryrun.app.dto.shared.LabelChangeDTO;
 import com.groceryrun.app.services.AisleService;
 import org.springframework.web.bind.annotation.*;
@@ -27,6 +28,16 @@ public class AisleController {
     @GetMapping("{id}")
     public AisleDTO getAisle(@PathVariable Integer id) {
         return aisleService.getAisleById(id);
+    }
+
+    @PostMapping("admin/{id}/categories")
+    public void addCategoryToAisle(@PathVariable Integer id, @RequestBody NewCategoryDTO newCategoryDTO) {
+        aisleService.addCategoryToAisle(id, newCategoryDTO);
+    }
+
+    @DeleteMapping("admin/{id}/categories/{categoryId}")
+    public void deleteCategoryFromAisle(@PathVariable Integer id, @PathVariable Integer categoryId) {
+        aisleService.deleteCategoryFromAisle(id, categoryId);
     }
 
     @PutMapping("{id}/label")
--- src/main/java/com/groceryrun/app/services/AisleService.java
+++ src/main/java/com/groceryrun/app/services/AisleService.java
@@ -5,8 +5,11 @@ import com.groceryrun.app.dto.aisle.AisleDTO;
 import com.groceryrun.app.dto.aisle.AisleDTOMapper;
 import com.groceryrun.app.dto.aisle.AisleStoreChangeDTO;
 import com.groceryrun.app.dto.shared.LabelChangeDTO;
+import com.groceryrun.app.dto.category.NewCategoryDTO;
 import com.groceryrun.app.entities.Aisle;
+import com.groceryrun.app.entities.Category;
 import com.groceryrun.app.repositories.AisleRepository;
+import com.groceryrun.app.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,19 +18,39 @@ import java.util.stream.Collectors;
 @Service
 public class AisleService {
     private final AisleRepository aisleRepository;
+    private final CategoryRepository categoryRepository;
     private final AisleDTOMapper aisleDTOMapper;
 
-    public AisleService(AisleRepository aisleRepository, AisleDTOMapper aisleDTOMapper) {
+    public AisleService(AisleRepository aisleRepository, AisleDTOMapper aisleDTOMapper, CategoryRepository categoryRepository) {
         this.aisleRepository = aisleRepository;
         this.aisleDTOMapper = aisleDTOMapper;
+        this.categoryRepository = categoryRepository;
     }
 
     public List<AisleDTO> getAllAisles() {
         return aisleRepository.findAll().stream().map(aisleDTOMapper).collect(Collectors.toList());
     }
 
+    public void addCategoryToAisle(Integer id, NewCategoryDTO newCategoryDTO) {
+        Aisle aisle = aisleRepository.findById(id).orElseThrow(() -> new IllegalStateException(id + " not found"));
+        Category category = new Category(newCategoryDTO.label(), aisle);
+        categoryRepository.save(category);
+        aisle.getCategories().add(category);
+        aisleRepository.save(aisle);
+    }
+
     public AisleDTO getAisleById(Integer id) {
         return aisleRepository.findById(id).map(aisleDTOMapper).orElseThrow(() -> new IllegalStateException(id + " not found"));
+    }
+
+    public void deleteCategoryFromAisle(Integer id, Integer categoryId) {
+        Aisle aisle = aisleRepository.findById(id).orElseThrow(() -> new IllegalStateException(id + " not found"));
+        Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new IllegalStateException(categoryId + " not found"));
+        if (aisle.getCategories().contains(category)) {
+            aisle.getCategories().remove(category);
+            aisleRepository.save(aisle);
+            categoryRepository.delete(category);
+        }
     }
 
     public void updateAisleLabel(Integer id, LabelChangeDTO labelChangeDTO) {
